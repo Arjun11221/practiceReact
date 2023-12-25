3147dbde4f90544fd08d6ebdbad713b081ec014c
Arjun <arjun346002@gmail.com>
2023-12-25 16:26:31 +0530

    testing1

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
     <div>
-        <h1>Hello This is Contact Page...</h1>
+      <h1 className="font-bold text-3xl m-4 p-4 ">
+        Hello This is Contact Page...
+      </h1>
+      <form className="">
+        <input
+          className=" p-2 m-2 border border-gray-400 outline-none rounded-md "
+          placeholder="name"
+        />
+        <input
+          className=" p-2 m-2 border border-gray-400 outline-none rounded-md "
+          placeholder="message"
+        />
+        <button className=" bg-slate-800 text-white p-2 m-2 border border-gray-400 outline-none rounded-md " >Submit</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
